90030cf6bb307ed670670986d6d8afc5f0fd3b90
JaiDing <dingxiaoshuai1@outlook.com>
2017-01-13 21:46:59 +0800

    commit the project to github

---
+++ app/src/main/java/com/mumu/textview_demo_1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mumu.textview_demo_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
